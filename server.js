002984d6d139db6512120b88122cd3d4e7aa724d
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-10-13 16:09:26 -0700

    Created location and API

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 require('dotenv').config();
-
-const express = require('express');
 const superagent = require('superagent');
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.use(cors());
 
@@ -16,8 +17,16 @@ app.get('/location', (request, response) => {
         console.log(request.query, request.body);
         const getLocation = require('./data/location.json');
         const search_query = request.query.city;
-        const newLocation = new Location(getLocation[0], search_query);
-        response.send(newLocation);
+        
+        const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+
+        superagent.get(url)
+        .then(data => {
+            const newLocation = data.body[0];
+            const locationData = new Location(city, getLocation);
+            console.log(locationData);
+            response.json(locationData)
+        });
     } catch {
         response.status(500).send('Sorry, something went wrong');
     }
@@ -31,7 +40,8 @@ function Location(city, search_query) {
 }
 
 app.get('/weather', (request, response) => {
-    const getWeather = require('./data/weather.json');
+    // const getWeather = require('./data/weather.json');
+    const url = `HTTPS: https://api.weatherbit.io/v2.0/forecast/daily?kry${WEATHER_API_KEY}&city=${undefined}`;
     const weatherArr = [];
     getWeather.data.map(weather => {
         const currentWeather = new Weather(weather);
