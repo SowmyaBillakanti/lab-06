08d3045edb0b867ceae2a3cd908acec90fcbeced
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-10-14 18:55:17 -0700

    Completed location with DB with small changes

--- server.js
+++ server.js
@@ -65,64 +65,64 @@ function Location(city, search_query) {
     this.longitude = search_query.lon;
 }
 
-// function Weather(data) {
-//     this.forecast = data.weather.description;
-//     this.time = data.datetime;
-// }
+function Weather(data) {
+    this.forecast = data.weather.description;
+    this.time = data.datetime;
+}
 
-// app.get('/weather', searchWeather)
-//     function searchWeather (request, response) {
-//         const lat = request.query.latitude;
-//         const lon = request.query.longitude;
-//         const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&days=8&lat=${lat}&lon=${lon}`;
+app.get('/weather', searchWeather)
+    function searchWeather (request, response) {
+        const lat = request.query.latitude;
+        const lon = request.query.longitude;
+        const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&days=8&lat=${lat}&lon=${lon}`;
     
-//         superagent.get(url)
-//         .then(result => {
-//             const dailyWeather = result.body.data.map(day => {
-//                 return new Weather(day);
-//             });
-//             response.send(dailyWeather);
-//         })
-//         .catch(err => console.error('weather returned error:', err));
-//     }
+        superagent.get(url)
+        .then(result => {
+            const dailyWeather = result.body.data.map(day => {
+                return new Weather(day);
+            });
+            response.send(dailyWeather);
+        })
+        .catch(err => console.error('weather returned error:', err));
+    }
 
-//     function Trails(data) {
-//         this.name = data.name;
-//         this.location = data.location;
-//         this.length = data.length;
-//         this.stars = data.stars;
-//         this.star_votes = data.starVotes;
-//         this.summary = data.summary;
-//         this.trail_url = data.trail_url;
-//         this.conditions = data.conditionStatus;
-//         this.condition_time = data.conditionDate.split(' ')[1];
-//         this.condition_date = data.conditionDate.split(' ')[0];
-//     }
+    function Trails(data) {
+        this.name = data.name;
+        this.location = data.location;
+        this.length = data.length;
+        this.stars = data.stars;
+        this.star_votes = data.starVotes;
+        this.summary = data.summary;
+        this.trail_url = data.trail_url;
+        this.conditions = data.conditionStatus;
+        this.condition_time = data.conditionDate.split(' ')[1];
+        this.condition_date = data.conditionDate.split(' ')[0];
+    }
 
     
-// app.get('/trails', searchTrails)
-//     function searchTrails (request, response) {
-//         const lat = request.query.latitude;
-//         const lon = request.query.longitude;
-//         // console.log(request.query);
-//         // console.log('latitude', lat);
-//         // console.log('longitude', lon);
-//         const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${TRAIL_API_KEY}`;
+app.get('/trails', searchTrails)
+    function searchTrails (request, response) {
+        const lat = request.query.latitude;
+        const lon = request.query.longitude;
+        // console.log(request.query);
+        // console.log('latitude', lat);
+        // console.log('longitude', lon);
+        const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${TRAIL_API_KEY}`;
 
-//         superagent.get(url)
-//         .then(result => {
-//             // console.log('+++++++++++++++++++++++++++++++', result.body.trails);
-//             const trailData = result.body.trails.map(trail => {
-//                 return new Trails(trail)
-//                })
-//             response.send(trailData);
-//         })
-//         .catch(err => console.error('Trail returned error:', err));
-//     }
+        superagent.get(url)
+        .then(result => {
+            // console.log('+++++++++++++++++++++++++++++++', result.body.trails);
+            const trailData = result.body.trails.map(trail => {
+                return new Trails(trail)
+               })
+            response.send(trailData);
+        })
+        .catch(err => console.error('Trail returned error:', err));
+    }
 
-// app.get('*', (request, response) => {
-//     response.status(500).send('Sorry something went wrong');
-// })
+app.get('*', (request, response) => {
+    response.status(500).send('Sorry something went wrong');
+})
 
 // connect to our database
 client.connect()
